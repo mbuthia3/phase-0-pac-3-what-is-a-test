37f77838e6deb3fd8e7eb53dfb7976dfb0f6af60
victor <victor.mbuthia@student.moringaschool.com>
2022-08-04 12:36:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const name = "Joe";
-const height = 74;
+const name = "viktor";
+const height = 35;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = {name, height, message};;
+/*
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('returns "viktor"', () => {
+      expect(name).toEqual('viktor')
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
+})
+*/
